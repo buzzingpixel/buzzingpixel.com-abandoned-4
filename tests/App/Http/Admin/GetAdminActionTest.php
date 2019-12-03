bedcef3ad6cf586bd56cd0bc569d651b79e68c53
TJ Draper <tj@buzzingpixel.com>
2019-12-02 20:55:18 -0600

    Add GetAdminActionTest

---
+++ tests/App/Http/Admin/GetAdminActionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Http\Admin;
+
+use App\Http\Admin\GetAdminAction;
+use PHPUnit\Framework\TestCase;
+use Tests\TestConfig;
+
+class GetAdminActionTest extends TestCase
+{
+    public function test() : void
+    {
+        $action = TestConfig::$di->get(GetAdminAction::class);
+
+        $response = $action();
+
+        self::assertSame(303, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+
+        self::assertCount(1, $headers);
+
+        $locationHeader = $headers['Location'];
+
+        self::assertCount(1, $locationHeader);
+
+        self::assertSame($locationHeader[0], '/admin/software');
+    }
+}
