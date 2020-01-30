4203e6af35c5330f058723f457384144e6416e99
TJ Draper <tj@buzzingpixel.com>
2020-01-29 20:26:18 -0600

    Add migration to add new fields to software tables

---
+++ config/Data/Migrations/20200128235446_add_new_software_fields.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/** @noinspection AutoloadingIssuesInspection */
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ClassFileName.NoMatch
+
+class AddNewSoftwareFields extends AbstractMigration
+{
+    public function change() : void
+    {
+        $this->table('software')
+            ->addColumn(
+                'price',
+                'float',
+                [
+                    'after' => 'is_for_sale',
+                    'default' => '0.0',
+                ]
+            )
+            ->addColumn(
+                'is_subscription',
+                'boolean',
+                [
+
+                    'after' => 'price',
+                    'default' => 0,
+                ]
+            )
+            ->update();
+
+        $this->table('software_versions')
+            ->addColumn(
+                'upgrade_price',
+                'float',
+                [
+                    'after' => 'download_file',
+                    'default' => 0,
+                ]
+            )
+            ->update();
+    }
+}
