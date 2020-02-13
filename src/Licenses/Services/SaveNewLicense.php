c86fa2522dcba6e4977f75d6c037aa6ecc2e9934
TJ Draper <tj@buzzingpixel.com>
2020-02-12 19:41:40 -0600

    Add service to save new license

---
+++ src/Licenses/Services/SaveNewLicense.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Licenses\Services;
+
+use App\Licenses\Models\LicenseModel;
+use App\Licenses\Transformers\TransformLicenseModelToRecord;
+use App\Payload\Payload;
+use App\Persistence\SaveNewRecord;
+use App\Persistence\UuidFactoryWithOrderedTimeCodec;
+use Exception;
+
+/**
+ * This should only be invoked from SaveLicenseMaster where the PDO transaction
+ * is begun and exception handling is in place
+ */
+class SaveNewLicense
+{
+    private SaveNewRecord $saveNewRecord;
+    private TransformLicenseModelToRecord $transformer;
+    private UuidFactoryWithOrderedTimeCodec $uuidFactory;
+
+    public function __construct(
+        SaveNewRecord $saveNewRecord,
+        TransformLicenseModelToRecord $transformer,
+        UuidFactoryWithOrderedTimeCodec $uuidFactory
+    ) {
+        $this->saveNewRecord = $saveNewRecord;
+        $this->transformer   = $transformer;
+        $this->uuidFactory   = $uuidFactory;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function __invoke(LicenseModel $model) : void
+    {
+        $model->id = $this->uuidFactory->uuid1()->toString();
+
+        $record = ($this->transformer)($model);
+
+        $payload = ($this->saveNewRecord)($record);
+
+        if ($payload->getStatus() === Payload::STATUS_CREATED) {
+            return;
+        }
+
+        throw new Exception('Unknown error saving license');
+    }
+}
---
+++ tests/Licenses/Services/SaveNewLicenseTest.php
@@ -0,0 +1,167 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Licenses\Services;
+
+use App\Licenses\Models\LicenseModel;
+use App\Licenses\Services\SaveNewLicense;
+use App\Licenses\Transformers\TransformLicenseModelToRecord;
+use App\Payload\Payload;
+use App\Persistence\Licenses\LicenseRecord;
+use App\Persistence\SaveNewRecord;
+use App\Persistence\UuidFactoryWithOrderedTimeCodec;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Tests\TestConfig;
+use Throwable;
+use function assert;
+
+class SaveNewLicenseTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testSaveNewLicense() : void
+    {
+        $uuid = TestConfig::$di->get(UuidFactoryWithOrderedTimeCodec::class)
+            ->uuid1();
+
+        $uuidFactory = $this->createMock(
+            UuidFactoryWithOrderedTimeCodec::class,
+        );
+
+        $uuidFactory->expects(self::once())
+            ->method('uuid1')
+            ->willReturn($uuid);
+
+        $saveNewRecord = $this->createMock(
+            SaveNewRecord::class,
+        );
+
+        $saveCallHolder = new stdClass();
+
+        $saveCallHolder->record = null;
+
+        $saveNewRecord->expects(self::once())
+            ->method('__invoke')
+            ->willReturnCallback(
+                static function (
+                    LicenseRecord $record
+                ) use ($saveCallHolder) : Payload {
+                    $saveCallHolder->record = $record;
+
+                    return new Payload(Payload::STATUS_CREATED);
+                }
+            );
+
+        $saveNewLicense = new SaveNewLicense(
+            $saveNewRecord,
+            new TransformLicenseModelToRecord(),
+            $uuidFactory,
+        );
+
+        $licenseModel = new LicenseModel();
+
+        $licenseModel->notes = 'U.S.S. Saratoga';
+
+        $saveNewLicense($licenseModel);
+
+        self::assertSame(
+            $uuid->toString(),
+            $licenseModel->id,
+        );
+
+        assert($saveCallHolder->record instanceof LicenseRecord);
+
+        self::assertSame(
+            $uuid->toString(),
+            $saveCallHolder->record->id,
+        );
+
+        self::assertSame(
+            $licenseModel->notes,
+            $saveCallHolder->record->notes,
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSaveNewLicenseInvalidPayloadReturnStatus() : void
+    {
+        $uuid = TestConfig::$di->get(UuidFactoryWithOrderedTimeCodec::class)
+            ->uuid1();
+
+        $uuidFactory = $this->createMock(
+            UuidFactoryWithOrderedTimeCodec::class,
+        );
+
+        $uuidFactory->expects(self::once())
+            ->method('uuid1')
+            ->willReturn($uuid);
+
+        $saveNewRecord = $this->createMock(
+            SaveNewRecord::class,
+        );
+
+        $saveCallHolder = new stdClass();
+
+        $saveCallHolder->record = null;
+
+        $saveNewRecord->expects(self::once())
+            ->method('__invoke')
+            ->willReturnCallback(
+                static function (
+                    LicenseRecord $record
+                ) use ($saveCallHolder) : Payload {
+                    $saveCallHolder->record = $record;
+
+                    return new Payload(Payload::STATUS_ERROR);
+                }
+            );
+
+        $saveNewLicense = new SaveNewLicense(
+            $saveNewRecord,
+            new TransformLicenseModelToRecord(),
+            $uuidFactory,
+        );
+
+        $licenseModel = new LicenseModel();
+
+        $licenseModel->notes = 'U.S.S. Saratoga';
+
+        $exception = null;
+
+        try {
+            $saveNewLicense($licenseModel);
+        } catch (Throwable $e) {
+            $exception = $e;
+        }
+
+        assert($exception instanceof Exception);
+
+        self::assertSame(
+            'Unknown error saving license',
+            $exception->getMessage(),
+        );
+
+        self::assertSame(
+            $uuid->toString(),
+            $licenseModel->id,
+        );
+
+        assert($saveCallHolder->record instanceof LicenseRecord);
+
+        self::assertSame(
+            $uuid->toString(),
+            $saveCallHolder->record->id,
+        );
+
+        self::assertSame(
+            $licenseModel->notes,
+            $saveCallHolder->record->notes,
+        );
+    }
+}
