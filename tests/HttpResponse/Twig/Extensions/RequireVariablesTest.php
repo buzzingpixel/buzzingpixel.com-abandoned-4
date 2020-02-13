cc566d408bfd21962866df37d7bf555b5e48e24f
TJ Draper <tj@buzzingpixel.com>
2020-02-12 22:39:46 -0600

    Add HTTP Response tests back in

---
+++ tests/HttpResponse/Twig/Extensions/RequireVariablesTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\HttpResponse\Twig\Extensions;
+
+use App\HttpResponse\Twig\Extensions\RequireVariables;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class RequireVariablesTest extends TestCase
+{
+    private RequireVariables $requireVariables;
+
+    protected function setUp() : void
+    {
+        $this->requireVariables = new RequireVariables();
+    }
+
+    public function testGetFunctions() : void
+    {
+        $return = $this->requireVariables->getFunctions();
+
+        $twigFunc = $return[0];
+
+        self::assertSame('requireVariables', $twigFunc->getName());
+
+        /** @var array $callable */
+        $callable = $twigFunc->getCallable();
+
+        self::assertSame($this->requireVariables, $callable[0]);
+
+        self::assertSame('requireVars', $callable[1]);
+
+        self::assertFalse($twigFunc->needsEnvironment());
+
+        self::assertTrue($twigFunc->needsContext());
+    }
+
+    public function testWhenVarDoesNotExist() : void
+    {
+        self::expectException(LogicException::class);
+
+        self::expectExceptionMessage(
+            'Variable "randomInstance" is required and must be of type "' . RandomInstance::class . '"'
+        );
+
+        $this->requireVariables->requireVars([], ['randomInstance' => RandomInstance::class]);
+    }
+
+    public function testWhenTypeIsNullAndVarExists() : void
+    {
+        $this->requireVariables->requireVars(
+            ['randomInstance' => 'foo'],
+            ['randomInstance' => null]
+        );
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testWhenVarTypeIsClassAndNotInstance() : void
+    {
+        self::expectException(LogicException::class);
+
+        self::expectExceptionMessage(
+            'Variable "randomInstance" is required and must be of type "' . RandomInstance::class . '"'
+        );
+
+        $this->requireVariables->requireVars(
+            ['randomInstance' => 'foo'],
+            ['randomInstance' => RandomInstance::class]
+        );
+    }
+
+    public function testWhenVarTypeIsClassAndIsInstance() : void
+    {
+        $this->requireVariables->requireVars(
+            ['randomInstance' => new RandomInstance()],
+            ['randomInstance' => RandomInstance::class]
+        );
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testWhenVarTypeIsStringAndInstanceNotString() : void
+    {
+        self::expectException(LogicException::class);
+
+        self::expectExceptionMessage(
+            'Variable "randomInstance" is required and must be of type "string"'
+        );
+
+        $this->requireVariables->requireVars(
+            ['randomInstance' => 23],
+            ['randomInstance' => 'string']
+        );
+    }
+
+    public function testWhenVarTypeIsStringAndInstanceIsString() : void
+    {
+        $this->requireVariables->requireVars(
+            ['randomInstance' => 'Foo'],
+            ['randomInstance' => 'string']
+        );
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testWhenVarTypeIsIntegerAndInstanceNotInteger() : void
+    {
+        self::expectException(LogicException::class);
+
+        self::expectExceptionMessage(
+            'Variable "randomInstance" is required and must be of type "integer"'
+        );
+
+        $this->requireVariables->requireVars(
+            ['randomInstance' => '23'],
+            ['randomInstance' => 'integer']
+        );
+    }
+
+    public function testWhenVarTypeIsIntegerAndInstanceIsInteger() : void
+    {
+        $this->requireVariables->requireVars(
+            ['randomInstance' => 123],
+            ['randomInstance' => 'integer']
+        );
+
+        $this->expectNotToPerformAssertions();
+    }
+}
