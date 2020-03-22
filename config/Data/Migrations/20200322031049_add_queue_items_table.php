1caf60487d4117136ca75d863355412a52225d96
TJ Draper <tj@buzzingpixel.com>
2020-03-21 22:16:01 -0500

    Add migrations to add queue tables

---
+++ config/Data/Migrations/20200322031049_add_queue_items_table.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+/** @noinspection AutoloadingIssuesInspection */
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ClassFileName.NoMatch
+
+class AddQueueItemsTable extends AbstractMigration
+{
+    public function change() : void
+    {
+        $this->table('queue_items', [
+            'id' => false,
+            'primary_key' => ['id'],
+        ])
+            ->addColumn(
+                'id',
+                'uuid',
+                ['comment' => 'UUID is generated in code'],
+            )
+            ->addColumn(
+                'queue_id',
+                'uuid'
+            )
+            ->addColumn(
+                'run_order',
+                'integer'
+            )
+            ->addColumn(
+                'is_finished',
+                'boolean',
+                ['default' => 0],
+            )
+            ->addColumn(
+                'finished_at',
+                'datetime',
+                [
+                    'null' => true,
+                    'timezone' => true,
+                ],
+            )
+            ->addColumn(
+                'class',
+                'string',
+            )
+            ->addColumn(
+                'method',
+                'string',
+            )
+            ->addColumn(
+                'context',
+                'json',
+                ['null' => true]
+            )
+            ->create();
+    }
+}
