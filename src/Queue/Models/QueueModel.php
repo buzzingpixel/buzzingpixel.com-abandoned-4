b7682877767a2e502b22a5b4353ce23b2e6363d5
TJ Draper <tj@buzzingpixel.com>
2020-03-27 21:57:40 -0500

    Update Queue Model to include a QueueItems property

--- src/Queue/Models/QueueModel.php
+++ src/Queue/Models/QueueModel.php
@@ -6,7 +6,13 @@ namespace App\Queue\Models;
 
 use DateTimeImmutable;
 use DateTimeZone;
+use RuntimeException;
+use function assert;
+use function is_array;
 
+/**
+ * @property QueueItemModel[] $Items
+ */
 class QueueModel
 {
     public function __construct()
@@ -47,4 +53,49 @@ class QueueModel
     public DateTimeImmutable $addedAt;
 
     public ?DateTimeImmutable $finishedAt = null;
+
+    /** @var QueueItemModel[] */
+    private array $items = [];
+
+    public function addItem(QueueItemModel $item) : void
+    {
+        $item->queue = $this;
+
+        $this->items[] = $item;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function __set(string $name, $value) : void
+    {
+        if ($name !== 'items') {
+            throw new RuntimeException('Property does not exist');
+        }
+
+        assert(is_array($value));
+
+        foreach ($value as $item) {
+            assert($item instanceof QueueItemModel);
+
+            $this->addItem($item);
+        }
+    }
+
+    public function __isset(string $name) : bool
+    {
+        return $name === 'items';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function __get(string $name)
+    {
+        if ($name !== 'items') {
+            throw new RuntimeException('Property does not exist');
+        }
+
+        return $this->items;
+    }
 }
