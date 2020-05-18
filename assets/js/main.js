ebceefd2f2be97abfc612875b338ddc9d7a47385
TJ Draper <tj@buzzingpixel.com>
2020-05-17 21:25:47 -0500

    Add JS to prevent empty submitting

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 import CartQuantity from './Components/CartQuantity.js';
 import ConfirmSubmit from './Components/ConfirmSubmit.js';
 import Flatpickr from './Components/Flatpickr.js';
+import PreventEmptySubmit from './Components/PreventEmptySubmit.js';
 import Selects from './Components/Selects.js';
 
 window.Methods.CartQuantity = CartQuantity;
@@ -18,6 +19,12 @@ const flatpickrEls = document.querySelectorAll('[ref="flatpickr"]');
 if (flatpickrEls.length > 0) {
     new Flatpickr(flatpickrEls);
 }
+
+// Prevent empty submit
+const emptySubmitEls = document.querySelectorAll('[ref="PreventEmptySubmit"]');
+emptySubmitEls.forEach((el) => {
+    new PreventEmptySubmit(el);
+});
 
 // Selects
 const selectEls = document.querySelectorAll('[ref="select"]');
