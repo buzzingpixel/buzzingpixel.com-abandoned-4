f0f55f217e5a68ebe41c3f61c310e335325fe6d5
TJ Draper <tj@buzzingpixel.com>
2020-06-25 19:05:34 -0500

    Use docker build kit in dev scripts

--- dev_scripts/bash/build.sh
+++ dev_scripts/bash/build.sh
@@ -3,7 +3,7 @@
 source ../../dev 2> /dev/null;
 
 function build() {
-    docker-compose ${composeFiles} -p buzzingpixel build;
+    COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose ${composeFiles} -p buzzingpixel build;
 
     return 0;
 }
