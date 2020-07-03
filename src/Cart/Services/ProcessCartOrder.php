345a370740ddbee5eceb6cdf16dfbe25e6a9a805
TJ Draper <tj@buzzingpixel.com>
2020-07-03 12:44:17 -0500

    Add process cart order service

---
+++ src/Cart/Services/ProcessCartOrder.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cart\Services;
+
+use App\Cart\Models\CartModel;
+use App\Cart\Models\ProcessOrderModel;
+use App\Cart\OrderProcessors\ChargeOrderToCard;
+use App\Cart\OrderProcessors\ClearCartAtEnd;
+use App\Cart\OrderProcessors\PopulateOrderAfterCharge;
+use App\Cart\OrderProcessors\SaveOrderAfterPopulate;
+use App\Orders\Models\OrderModel;
+use App\Payload\Payload;
+use App\Persistence\UuidFactoryWithOrderedTimeCodec;
+use App\Users\Models\UserCardModel;
+use League\Pipeline\Pipeline;
+use Psr\Container\ContainerInterface;
+use Throwable;
+
+use function assert;
+
+class ProcessCartOrder
+{
+    private ContainerInterface $di;
+
+    public function __construct(ContainerInterface $di)
+    {
+        $this->di = $di;
+    }
+
+    public function __invoke(CartModel $cart, UserCardModel $card): Payload
+    {
+        try {
+            return $this->innerRun($cart, $card);
+        } catch (Throwable $e) {
+            return new Payload(Payload::STATUS_ERROR);
+        }
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function innerRun(CartModel $cart, UserCardModel $card): Payload
+    {
+        $uuidFactory = $this->di->get(UuidFactoryWithOrderedTimeCodec::class);
+        assert($uuidFactory instanceof UuidFactoryWithOrderedTimeCodec);
+
+        $order = new OrderModel();
+
+        $order->user = $cart->user;
+
+        $order->id = $uuidFactory->uuid1()->toString();
+
+        $processOrderModel = new ProcessOrderModel(
+            $cart,
+            $card,
+            $order,
+        );
+
+        $di = $this->di;
+
+        $chargeOrderToCard = $di->get(ChargeOrderToCard::class);
+        assert($chargeOrderToCard instanceof ChargeOrderToCard);
+
+        $populateOrderAfterCharge = $di->get(PopulateOrderAfterCharge::class);
+        assert($populateOrderAfterCharge instanceof PopulateOrderAfterCharge);
+
+        $saveOrderAfterPopulate = $di->get(SaveOrderAfterPopulate::class);
+        assert($saveOrderAfterPopulate instanceof SaveOrderAfterPopulate);
+
+        $clearCartAtEnd = $di->get(ClearCartAtEnd::class);
+        assert($clearCartAtEnd instanceof ClearCartAtEnd);
+
+        // TODO: Create subscriptions
+        $pipeline = (new Pipeline())
+            ->pipe($chargeOrderToCard)
+            ->pipe($populateOrderAfterCharge)
+            ->pipe($saveOrderAfterPopulate)
+            ->pipe($clearCartAtEnd);
+
+        $pipeline->process($processOrderModel);
+
+        return new Payload(
+            Payload::STATUS_SUCCESSFUL,
+            ['orderId' => $order->id]
+        );
+    }
+}
