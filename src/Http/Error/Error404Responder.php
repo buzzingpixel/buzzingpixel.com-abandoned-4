e12b9ccd42b9421783999b3562960a53ab523de9
TJ Draper <tj@buzzingpixel.com>
2020-07-25 14:28:12 -0500

    Implement 404 and 500 error pages

--- src/Http/Error/Error404Responder.php
+++ src/Http/Error/Error404Responder.php
@@ -4,21 +4,26 @@ declare(strict_types=1);
 
 namespace App\Http\Error;
 
+use App\Content\Meta\MetaPayload;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Twig\Environment as TwigEnvironment;
 
 class Error404Responder
 {
     private ResponseFactoryInterface $responseFactory;
+    private TwigEnvironment $twigEnvironment;
 
-    public function __construct(ResponseFactoryInterface $responseFactory)
-    {
+    public function __construct(
+        ResponseFactoryInterface $responseFactory,
+        TwigEnvironment $twigEnvironment
+    ) {
         $this->responseFactory = $responseFactory;
+        $this->twigEnvironment = $twigEnvironment;
     }
 
     public function __invoke(): ResponseInterface
     {
-        // TODO: Implement 404 page
         $response = $this->responseFactory->createResponse(
             404,
             'Page not found'
@@ -26,7 +31,14 @@ class Error404Responder
             // We'll statically cache the response so 404s can't DDOS us
             ->withHeader('EnableStaticCache', 'true');
 
-        $response->getBody()->write('Page not found');
+        $response->getBody()->write($this->twigEnvironment->render(
+            'Http/Errors/404.twig',
+            [
+                'metaPayload' => new MetaPayload(
+                    ['metaTitle' => 'Page not found']
+                ),
+            ]
+        ));
 
         return $response;
     }
--- src/Http/Error/Error500Responder.php
+++ src/Http/Error/Error500Responder.php
@@ -4,21 +4,26 @@ declare(strict_types=1);
 
 namespace App\Http\Error;
 
+use App\Content\Meta\MetaPayload;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 use Throwable;
+use Twig\Environment as TwigEnvironment;
 
 class Error500Responder
 {
     private ResponseFactoryInterface $responseFactory;
+    protected TwigEnvironment $twigEnvironment;
     private LoggerInterface $logger;
 
     public function __construct(
         ResponseFactoryInterface $responseFactory,
+        TwigEnvironment $twigEnvironment,
         LoggerInterface $logger
     ) {
         $this->responseFactory = $responseFactory;
+        $this->twigEnvironment = $twigEnvironment;
         $this->logger          = $logger;
     }
 
@@ -29,7 +34,6 @@ class Error500Responder
             ['exception' => $exception]
         );
 
-        // TODO: Implement 500 page
         $response = $this->responseFactory->createResponse(
             500,
             'An internal server error occurred'
@@ -37,7 +41,14 @@ class Error500Responder
             // We'll statically cache the response so 500s can't DDOS us
             ->withHeader('EnableStaticCache', 'true');
 
-        $response->getBody()->write('An internal server error occurred');
+        $response->getBody()->write($this->twigEnvironment->render(
+            'Http/Errors/500.twig',
+            [
+                'metaPayload' => new MetaPayload(
+                    ['metaTitle' => 'An internal server error occurred']
+                ),
+            ]
+        ));
 
         return $response;
     }
